3f452ed70838e9f11809572ec55b23879e4e2788
MDTanvirAhmedEmon <mdtanvirahmedemon17@gmail.com>
2025-06-12 19:47:42 +0600

    done geometry working on texture

---
+++ src/script.js
@@ -0,0 +1,231 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { Timer } from 'three/addons/misc/Timer.js'
+import GUI from 'lil-gui'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+// Textures loader
+const textureLoader = new THREE.TextureLoader();
+
+// floor texture
+const floorAlphaTexture = textureLoader.load('./floor/alpha.jpg')
+const floorColorTexture = textureLoader.load('./floor/coast_sand_rocks_02_diff_1k.jpg')
+const floorARMTexture = textureLoader.load('./floor/coast_sand_rocks_02_arm_1k.jpg')
+const floorNormalTexture = textureLoader.load('./floor/coast_sand_rocks_02_nor_gl_1k.jpg')
+const floorDisplacementTexture = textureLoader.load('./floor/coast_sand_rocks_02_disp_1k.jpg')
+
+floorColorTexture.colorSpace = THREE.SRGBColorSpace
+floorColorTexture.repeat.set(8, 8)
+floorColorTexture.wrapS = THREE.RepeatWrapping
+floorColorTexture.wrapT = THREE.RepeatWrapping
+
+floorARMTexture.repeat.set(8, 8)
+floorARMTexture.wrapS = THREE.RepeatWrapping
+floorARMTexture.wrapT = THREE.RepeatWrapping
+
+floorNormalTexture.repeat.set(8, 8)
+floorNormalTexture.wrapS = THREE.RepeatWrapping
+floorNormalTexture.wrapT = THREE.RepeatWrapping
+
+floorDisplacementTexture.repeat.set(8, 8)
+floorDisplacementTexture.wrapS = THREE.RepeatWrapping
+floorDisplacementTexture.wrapT = THREE.RepeatWrapping
+
+/**
+ * House
+ */
+const house = new THREE.Group();
+scene.add(house)
+
+const walls = new THREE.Mesh(
+    new THREE.BoxGeometry(4, 2.5, 4),
+    new THREE.MeshStandardMaterial()
+)
+
+const roof = new THREE.Mesh(
+    new THREE.ConeGeometry(3.5, 1.5, 4, 1),
+    new THREE.MeshStandardMaterial()
+)
+
+const door = new THREE.Mesh(
+    new THREE.PlaneGeometry(2.2, 2.2),
+    new THREE.MeshStandardMaterial()
+)
+
+house.add(walls)
+walls.position.y = 1.26
+
+house.add(roof)
+roof.position.y = 2.5 + 0.75
+roof.rotation.y = Math.PI / 4
+
+house.add(door)
+door.position.z = 2 + 0.001;
+door.position.y = 1.1;
+door.rotation.y = 0;
+
+
+const plane = new THREE.Mesh(
+    new THREE.PlaneGeometry(20, 20, 100, 100),
+    new THREE.MeshStandardMaterial({
+        alphaMap: floorAlphaTexture,
+        transparent: true,
+        side: THREE.DoubleSide,
+        map: floorColorTexture,
+        aoMap: floorARMTexture,
+        roughnessMap: floorARMTexture,
+        metalnessMap: floorARMTexture,
+        normalMap: floorNormalTexture,
+        displacementMap: floorDisplacementTexture,
+        displacementScale: 0.3,     // Lower scale to reduce effect
+        displacementBias: -0.2,        // No vertical offset
+    })
+)
+scene.add(plane)
+plane.position.set(0, 0, 0)
+plane.rotation.x = Math.PI / 2;
+
+// Bushes
+// ======================
+const bushesGeometry = new THREE.SphereGeometry(1, 16, 16);
+const bushesMaterial = new THREE.MeshStandardMaterial();
+const bushes = new THREE.Mesh(bushesGeometry, bushesMaterial)
+bushes.position.set(0.8, 0.2, 2.2)
+bushes.scale.set(0.5, 0.5, 0.5)
+house.add(bushes)
+
+const bushes2 = new THREE.Mesh(bushesGeometry, bushesMaterial)
+bushes2.scale.set(0.25, 0.25, 0.25)
+bushes2.position.set(1.4, 0.1, 2.1)
+house.add(bushes2)
+
+const bushes3 = new THREE.Mesh(bushesGeometry, bushesMaterial)
+bushes3.scale.set(0.4, 0.4, 0.4)
+bushes3.position.set(-0.8, 0.1, 2.2)
+house.add(bushes3)
+
+const bushes4 = new THREE.Mesh(bushesGeometry, bushesMaterial)
+bushes4.scale.set(0.15, 0.15, 0.15)
+bushes4.position.set(-1, 0.05, 2.6)
+house.add(bushes4)
+
+// Graves
+// ========================
+const graveGeometry = new THREE.BoxGeometry(0.6, 0.8, 0.2);
+const graveMaterial = new THREE.MeshStandardMaterial();
+
+const graves = new THREE.Group();
+scene.add(graves)
+
+for (let i = 0; i < 30; i++) {
+    const angle = Math.random() * Math.PI * 2
+    const radius = 3 + Math.random() * 4
+    const x = Math.sin(angle) * radius
+    const z = Math.cos(angle) * radius
+    const grave = new THREE.Mesh(graveGeometry, graveMaterial)
+    grave.position.x = x
+    grave.position.z = z
+    grave.position.y = Math.random() * 0.4
+
+    grave.rotation.x = (Math.random() - 0.5) * 0.4
+    grave.rotation.y = (Math.random() - 0.5) * 0.4
+    grave.rotation.z = (Math.random() - 0.5) * 0.4
+    graves.add(grave)
+}
+
+
+/**
+ * Lights
+ */
+// Ambient light
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.1)
+scene.add(ambientLight)
+
+// Directional light
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1.5)
+directionalLight.position.set(8, 4, 1)
+scene.add(directionalLight)
+
+console.log(directionalLight);
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 1)
+scene.add(directionalLightHelper)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () => {
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 5
+camera.position.z = 12
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const timer = new Timer()
+
+const tick = () => {
+    // Timer
+    timer.update()
+    const elapsedTime = timer.getElapsed()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
